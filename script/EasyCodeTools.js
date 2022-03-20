f6a03765aef6f1dc9878397efa086195e910954a
砸松果 <zasongguo@outlook.com>
2022-03-20 13:45:20 +0800

    修改抽奖概率

--- script/EasyCodeTools.js
+++ script/EasyCodeTools.js
@@ -1,4 +1,5 @@
 console.log( "EasyCodeTools JavaScript is running!" );
+//操作元素
 function changeElement(ElementID,Text,Disable){
     document.getElementById(ElementID).innerHTML=Text;
     if (Disable == true){
@@ -7,4 +8,9 @@ function changeElement(ElementID,Text,Disable){
     else if (Disable == false){
         document.getElementById(ElementID).style="display:inline;";
     }
+}
+//获取表单数据
+function getForm(FormId){
+    var FORMVALUE = document.getElementById(FormId).value;
+    FORMVALUE.toString();
 }

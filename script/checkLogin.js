b1f029f4a9c57a9dd47390d27cdb557c524aa987
砸松果 <zasongguo@outlook.com>
2022-03-26 23:08:58 +0800

    增加日期检测

--- script/checkLogin.js
+++ script/checkLogin.js
@@ -1,7 +1,12 @@
 window.setInterval("test()",1);
 function test(){
     var	iflogin = localStorage.getItem("iflogin");
-    if (iflogin != "yes"){
+    if (iflogin != "true"){
         window.location.href="../index.html";
     }
+}
+//判断日期是否属于开放时间
+var today = new Date().getDay();
+if (today != "6" && today !="0"){
+    window.location.href="../closetime.html";
 }
